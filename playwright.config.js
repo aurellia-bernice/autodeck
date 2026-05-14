7d8dddb393bfc2e6154a283cc1cdca6fbca0b0b7
Isabella Nweze <nwezeisabella@gmail.com>
2026-05-14 17:35:36 +0000

    Workflow fix

--- playwright.config.js
+++ playwright.config.js
@@ -2,8 +2,9 @@ const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: './tests',
-  fullyParallel: false,
-  retries: process.env.CI ? 2 : 0,
+  fullyParallel: true,
+  workers: process.env.CI ? 2 : undefined,
+  retries: process.env.CI ? 1 : 0,
   reporter: [['html', { open: 'never' }], ['list']],
   use: {
     baseURL: 'http://localhost:8081',
